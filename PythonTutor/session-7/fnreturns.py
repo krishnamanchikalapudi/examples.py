228ea10c80158b89ba98850ddec616246571967c
Krishna Manchikalapudi <krishna.manchikalapudi@gmail.com>
2020-06-17 19:09:45 -0700

    modified:   PythonTutor/session-7/fnreturns.py 	modified:   PythonTutor/session-9/classobject.py 	MachineLearning/docs/reinforcement/ 	MachineLearning/docs/supervised/ 	MachineLearning/docs/unsupervised/ 	PythonTutor/session-9/vehicleobject-1.py 	PythonTutor/session-9/vehicleobject-2.py 	PythonTutor/session-9/vehicleobject-3.py

--- PythonTutor/session-7/fnreturns.py
+++ PythonTutor/session-7/fnreturns.py
@@ -3,17 +3,28 @@ Session: 7
 Topic: Function returns
 """
 
+p = 10
+
 def calculator (num1, num2, oper):
+    q= 20
+    print (p)
+    print(q)
     if (oper == "+"):
-       return add(num1, num2)
+        print(p)
+        print (q)
+        return add(num1, num2)
 
 def add(num1, num2):
+    print(p)
+    # print(q)
     num = (num1 + num2)
     print (num)
-    # return num
+    return num
+
 
 
 # print ('5 + 10 = {} '. format( calculator(5, 10, "+") ) )
 
 addv = add(10, 20)
 print ('10 + 20 = {} '. format( addv ) )
+print(p)
--- PythonTutor/session-9/classobject.py
+++ PythonTutor/session-9/classobject.py
@@ -3,25 +3,42 @@ Session: 9
 Topic: Class
 """
 class MyClass:
-    x = 100
+    x = 100 # global variable inside class
 
     # constructor
     def __init__(self):
+        print("init X = {}".format(self.x))
         print('Class initialize')
+
+
+    # constructor with variable
+    def __init__(self, abc):
+        print("init X = {}".format(self.x))
+        print('Class initialize with value {} '. format(abc))
 
     @classmethod
     def hello(self):
+        y = 200 # global variable inside function only
+        print ("hello X = {}". format(self.x))
+        print("hello Y = {}".format(y))
         return 'Hello World!'
 
     @classmethod
     def add(self, num1, num2):
+        print("add X = {}".format(self.x))
         return (num1 + num2)
 
     # destructor or deletes class
     def __del__(self):
+        print("X = {}".format(self.x))
         print('destroy object')
 
-c = MyClass()
-print('X is {}'.format(c.x))
+
+c = MyClass('arg1')
+
+"""
+# print('X is {}'.format(self.x))
+print('c.X is {}'.format(c.x))
 print('hello method value is {}'.format(c.hello()))
 print('add method value is {}'.format(c.add(10, 20)))
+"""
